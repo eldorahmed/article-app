1ad47c00e90314514922d3bb6a0aaaa3b12ec814
eldorahmed <eldorbek970304@gmail.com>
2024-06-24 19:53:33 +0500

    ready

--- src/pages/ArticlesDetails.jsx
+++ src/pages/ArticlesDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch"
+import { Link } from "react-router-dom"
 function ArticlesDetails() {
     const {id}=useParams()
     const url=`https://online-json-server-api.up.railway.app/project/66793dd61d2cd3eb1144093b/articles?id=${id}`
@@ -10,10 +11,11 @@ function ArticlesDetails() {
     {isPending && <h2>Loading..</h2>}
       {error && <h2>{error}</h2>}
       {article&& <>
-      <img height={350} src={article[0].image} alt={article[0].title} />
-      <h3 className="text-3xl font-medium">{article[0].title}</h3>
+      <img className="mb-8" height={350} src={article[0].image} alt={article[0].title} />
+      <h3 className="text-3xl font-medium mb-4">{article[0].title}</h3>
       <p className="text-left">{article[0].body}</p>
       <p className="text-right mr-32 text-2xl font-medium"><i>{article[0].author}</i></p>
+      <Link to="/articles" className="btn btn-outline">Back to Articles</Link>
 
       </>}
 
